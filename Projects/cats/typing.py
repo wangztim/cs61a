7f3498df96d2333b57173af2f133f8be70776529
Tim Wang <timzwang@outlook.com>
2019-09-25 10:32:30 -0700

    finished cats project

--- Projects/cats/typing.py
+++ Projects/cats/typing.py
@@ -17,6 +17,14 @@ def choose(paragraphs, select, k):
     """
     # BEGIN PROBLEM 1
     "*** YOUR CODE HERE ***"
+    selectable_paragraphs = []
+    for paragraph in paragraphs:
+        if select(paragraph) == True:
+            selectable_paragraphs.append(paragraph)
+    if len(selectable_paragraphs) > k:
+        return selectable_paragraphs[k]
+    else:
+        return ""
     # END PROBLEM 1
 
 
@@ -33,6 +41,17 @@ def about(topic):
     assert all([lower(x) == x for x in topic]), 'topics should be lowercase.'
     # BEGIN PROBLEM 2
     "*** YOUR CODE HERE ***"
+    def check_if_about(paragraph):
+        paragraph_in_lower_case = lower(paragraph)
+        lower_case_paragraph_without_punc = remove_punctuation(
+            paragraph_in_lower_case)
+        words_of_paragraph = split(lower_case_paragraph_without_punc)
+        for keyword in topic:
+            if keyword in words_of_paragraph:
+                return True
+        return False
+
+    return check_if_about
     # END PROBLEM 2
 
 
@@ -57,6 +76,18 @@ def accuracy(typed, reference):
     reference_words = split(reference)
     # BEGIN PROBLEM 3
     "*** YOUR CODE HERE ***"
+    num_typed_words = len(typed_words)
+    if num_typed_words == 0:
+        return 0.0
+    smaller_len = min(num_typed_words, len(reference_words))
+    corrects = 0
+    for i in range(0, smaller_len):
+        typed_word = typed_words[i]
+        reference_word = reference_words[i]
+        if typed_word == reference_word:
+            corrects += 1
+    return (corrects / num_typed_words) * 100
+
     # END PROBLEM 3
 
 
@@ -65,6 +96,10 @@ def wpm(typed, elapsed):
     assert elapsed > 0, 'Elapsed time must be positive'
     # BEGIN PROBLEM 4
     "*** YOUR CODE HERE ***"
+    num_chars = len(typed)
+    num_words = num_chars / 5
+
+    return num_words * (60 / elapsed)
     # END PROBLEM 4
 
 
@@ -75,7 +110,25 @@ def autocorrect(user_word, valid_words, diff_function, limit):
     """
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
-    # END PROBLEM 5
+    if user_word in valid_words:
+        return user_word
+    else:
+        lowest_diff = float('inf')
+        best_word = ""
+        for word in valid_words:
+            difference = diff_function(user_word, word, limit)
+            if (difference == True):
+                difference = 1
+            if (difference == False):
+                difference = 0
+            if (difference < lowest_diff):
+                lowest_diff = difference
+                best_word = word
+        if lowest_diff > limit:
+            return user_word
+        else:
+            return best_word
+        # END PROBLEM 5
 
 
 def swap_diff(start, goal, limit):
@@ -84,37 +137,52 @@ def swap_diff(start, goal, limit):
     their lengths.
     """
     # BEGIN PROBLEM 6
-    assert False, 'Remove this line'
+    trimmed_start = start[1:] if len(start) > 1 else ""
+    trimmed_goal = goal[1:] if len(goal) > 1 else ""
+
+    if start == goal:
+        return 0
+    elif len(start) == 0 or len(goal) == 0:
+        return max(len(start), len(goal))
+    elif limit == 0:
+        return float('inf')
+    elif start[0] == goal[0]:
+        return swap_diff(trimmed_start, trimmed_goal, limit)
+    else:
+        return 1 + swap_diff(trimmed_start, trimmed_goal, limit - 1)
     # END PROBLEM 6
+
 
 def edit_diff(start, goal, limit):
     """A diff function that computes the edit distance from START to GOAL."""
-    assert False, 'Remove this line'
+    trimmed_start = start[1:] if len(start) > 1 else ""
+    trimmed_goal = goal[1:] if len(goal) > 1 else ""
 
-    if ______________: # Fill in the condition
-        # BEGIN
-        "*** YOUR CODE HERE ***"
-        # END
+    if start == goal:
+        return 0
 
-    elif ___________: # Feel free to remove or add additional cases
-        # BEGIN
-        "*** YOUR CODE HERE ***"
-        # END
+    elif len(start) == 0 or len(goal) == 0:
+        return max(len(start), len(goal))
+
+    elif limit == 0:
+        return float('inf')
+
+    elif start[0] == goal[0]:
+        return edit_diff(trimmed_start,  trimmed_goal, limit)
 
     else:
-        add_diff = ...  # Fill in these lines
-        remove_diff = ... 
-        substitute_diff = ... 
+        add_diff = edit_diff(start, trimmed_goal, limit - 1)
+        remove_diff = edit_diff(trimmed_start, goal, limit - 1)
+        substitute_diff = edit_diff(trimmed_start, trimmed_goal, limit - 1)
         # BEGIN
         "*** YOUR CODE HERE ***"
+        return 1 + min(add_diff, remove_diff, substitute_diff)
         # END
 
 
 def final_diff(start, goal, limit):
     """A diff function. If you implement this function, it will be used."""
     assert False, 'Remove this line to use your final_diff function'
-
-
 
 
 ###########
@@ -126,6 +194,16 @@ def report_progress(typed, prompt, id, send):
     """Send a report of your id and progress so far to the multiplayer server."""
     # BEGIN PROBLEM 8
     "*** YOUR CODE HERE ***"
+    words_correct = 0
+    for i in range(len(typed)):
+        if i < len(prompt):
+            if typed[i] == prompt[i]:
+                words_correct += 1
+            else:
+                break
+    words_accuracy = words_correct / len(prompt)
+    send({"id": id, "progress": words_accuracy})
+    return words_accuracy
     # END PROBLEM 8
 
 
@@ -147,6 +225,33 @@ def fastest_words(word_times, margin=1e-5):
     assert margin > 0
     # BEGIN PROBLEM 9
     "*** YOUR CODE HERE ***"
+    best_scores = []
+    for i in range(n_players):
+        best_scores.append([])
+
+    def find_diff_in_time(prev_list, curr_list):
+        current_elapsed_time = elapsed_time(curr_list)
+        previous_elapsed_time = elapsed_time(prev_list)
+        diff = current_elapsed_time - previous_elapsed_time
+        return abs(diff)
+
+    for typed_word in range(1, n_words + 1):
+        fastest_speed = float('inf')
+        for player in range(n_players):
+            current_word_list = word_times[player][typed_word]
+            previous_word_list = word_times[player][typed_word - 1]
+            diff = find_diff_in_time(previous_word_list, current_word_list)
+            if diff < fastest_speed:
+                fastest_speed = diff
+
+        for player in range(n_players):
+            current_word_list = word_times[player][typed_word]
+            previous_word_list = word_times[player][typed_word - 1]
+            diff = find_diff_in_time(previous_word_list, current_word_list)
+            if (abs(diff - fastest_speed) <= margin) == True:
+                best_scores[player].append(word(current_word_list))
+
+    return best_scores
     # END PROBLEM 9
 
 
@@ -176,7 +281,7 @@ enable_multiplayer = False  # Change to True when you
 def run_typing_test(topics):
     """Measure typing speed and accuracy on the command line."""
     paragraphs = lines_from_file('data/sample_paragraphs.txt')
-    select = lambda p: True
+    def select(p): return True
     if topics:
         select = about(topics)
     i = 0
